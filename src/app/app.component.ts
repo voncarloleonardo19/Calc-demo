ccd89639beb4f35634e4f7f57bf8b51b60d92b9a
voncarloleonardo19 <voncarloleonardo19@gmail.com>
2019-07-08 21:30:38 +0800

    Commit from local to Dev

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+
+  title = 'Calculator App';
+
+  num1: number;
+
+  num2: number;
+
+  result: number;
+
+  add() {
+
+      this.result = this.num1 + this.num2;
+
+  }
+
+  substract() {
+
+      this.result = this.num1 - this.num2;
+
+  }
+
+  multiply() {
+
+      this.result = this.num1 * this.num2;
+
+  }
+
+  divide() {
+
+      this.result = this.num1 % this.num2;
+
+  }
+
+}
